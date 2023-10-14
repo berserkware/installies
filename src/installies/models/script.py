bba19a93febf27d0f9af6cc25f61f20f38478cdb
Berserkware <caleb@berserkware.org>
2023-10-14 16:51:36 +1300

    Updated forms

--- src/installies/models/script.py
+++ src/installies/models/script.py
@@ -118,7 +118,6 @@ class Script(BaseModel):
             shell: Shell,
             submitter: User,
             use_default_function_matcher: bool=True,
-            **kwargs,
     ):
         """
         Create a Script object, and adds it to the database.
@@ -325,9 +324,7 @@ class AppScript(BaseModel):
     thread = ForeignKeyField(Thread, backref='for_script')
 
     @classmethod
-    def create(cls, app, version=None, **kwargs):
-        script = Script.create(**kwargs)
-
+    def create(cls, script: Script, app, version=None):
         thread = Thread.create(
             title=f'Discussion of script: "{script.description}"',
             creator=None,
